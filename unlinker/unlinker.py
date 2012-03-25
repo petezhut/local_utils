6352357a89e81af275e0b193cd2eb3b0ed57e8be
petezhut <pete.zhut@gmail.com>
2012-03-24 20:38:58 -0400

    Added copy|move capability to the unlinker.

--- unlinker/unlinker.py
+++ unlinker/unlinker.py
@@ -4,13 +4,19 @@ import os
 import sys
 import shutil
 
-def moveLink(item):
+def handleLink(item, action):
     try:
         if os.path.islink(item):
             rp = os.path.realpath(item)
-            print("%s is a link to: %s" % (item, os.path.realpath(item)))
             os.unlink(item)
-            shutil.move(rp, item)
+            print("%s is a link to: %s" % (item, os.path.realpath(item)))
+            if action == 'copy':
+                if os.path.isdir(item):
+                    shutil.copytree(rp, item)
+                else:
+                    shutil.copy(rp, item)
+            elif action == 'move':
+                shutil.move(rp, item)
         else:
             return
     except IOError, e:
@@ -19,16 +25,21 @@ def moveLink(item):
 def readDir(**kwargs):
     startingDir = kwargs.get('startingDir', os.getcwd())
     os.chdir(kwargs['dir'])
-    print("Current Dir: %s" % (os.path.abspath(os.getcwd())))
+    print("Current Working Dir: %s" % (os.path.abspath(os.getcwd())))
     for item in os.listdir("."):
         if os.path.isdir(item): 
             readDir(dir=item, startingDir=startingDir)
-        moveLink(item)
+        handleLink(item, kwargs['action'])
     os.chdir(startingDir)
 
 if __name__ == '__main__':
-    if len(sys.argv[1:]) <> 1:
-        dir = "."
-    else:
-        dir = sys.argv[-1]
-    readDir(dir=dir)
+    try:
+        action = sys.argv[1]
+        dir = sys.argv[2]
+    except:
+        if sys.argv[1].strip().lower() not in ['copy', 'move']:
+            print("You need an action (copy|move) for this program")
+            sys.exit(1)
+        else:
+            dir = "."
+    readDir(action=action.strip().lower(), dir=dir)
